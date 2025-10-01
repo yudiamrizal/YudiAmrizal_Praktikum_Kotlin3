618b017f6ab55e3cb3c4b553da13506d15435299
yudi amrizal <yudiamrizalpnb@gmail.com>
2025-10-01 13:24:18 +0700

    Commit

---
+++ src/id/ac/polbeng/yudiamrizal/test_kelas/Prak3_PersonC.kt
@@ -0,0 +1,9 @@
+package id.ac.polbeng.yudiamrizal.test_kelas
+
+class PersonC (var firstName: String, var lastName: String, var age: Int) {}
+
+fun main(){
+    val budi = PersonC("Budi", "Gunawan", 21)
+    println("Name : ${budi.firstName} ${budi.lastName}")
+    println("Age : ${budi.age}")
+}
