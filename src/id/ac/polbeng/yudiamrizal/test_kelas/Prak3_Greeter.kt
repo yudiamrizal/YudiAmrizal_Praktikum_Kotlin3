2c9e766658301be627f32e3a4fccd5e2e67ad1de
yudi amrizal <yudiamrizalpnb@gmail.com>
2025-10-01 13:08:35 +0700

    Commit

---
+++ src/id/ac/polbeng/yudiamrizal/test_kelas/Prak3_Greeter.kt
@@ -0,0 +1,7 @@
+package id.ac.polbeng.yudiamrizal.test_kelas
+
+class Greeter {
+    fun greet() {
+        println("Hello object world!")
+    }
+}
---
+++ src/id/ac/polbeng/yudiamrizal/test_kelas/Prak3_GreeterTest.kt
@@ -0,0 +1,6 @@
+package id.ac.polbeng.yudiamrizal.test_kelas
+
+fun main() {
+    val greeter = Greeter()
+    greeter.greet()
+}
