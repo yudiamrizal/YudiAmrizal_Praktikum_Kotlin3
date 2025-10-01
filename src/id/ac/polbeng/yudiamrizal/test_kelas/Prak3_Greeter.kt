3801333f57dac89db5d0ee417e292d45e347b536
yudi amrizal <yudiamrizalpnb@gmail.com>
2025-10-01 13:11:43 +0700

    Commit

--- src/id/ac/polbeng/yudiamrizal/test_kelas/Prak3_Greeter.kt
+++ src/id/ac/polbeng/yudiamrizal/test_kelas/Prak3_Greeter.kt
@@ -1,7 +1,20 @@
 package id.ac.polbeng.yudiamrizal.test_kelas
-
+/*
 class Greeter {
     fun greet() {
         println("Hello object world!")
+    }
+}
+ */
+class Greeter {
+    var text: String = ""
+    fun greet() {
+        println("Hello object world!")
+    }
+    fun greet(name: String) {
+        println("$text $name")
+    }
+    fun with_ret_val(name: String): String {
+        return "$text $name"
     }
 }
--- src/id/ac/polbeng/yudiamrizal/test_kelas/Prak3_GreeterTest.kt
+++ src/id/ac/polbeng/yudiamrizal/test_kelas/Prak3_GreeterTest.kt
@@ -1,6 +1,17 @@
 package id.ac.polbeng.yudiamrizal.test_kelas
-
+/*
 fun main() {
     val greeter = Greeter()
     greeter.greet()
+}
+
+ */
+fun main() {
+    val greeter = Greeter()
+    greeter.greet()
+    greeter.text = "Hi"
+    greeter.greet("Anton")
+    greeter.greet("Budi")
+    greeter.text = "Hello programmer"
+    println(greeter.with_ret_val("Dono"))
 }
