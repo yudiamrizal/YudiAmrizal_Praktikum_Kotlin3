33a992ddbd5cebbae47294196b303d2dcc081bc1
yudi amrizal <yudiamrizalpnb@gmail.com>
2025-10-01 13:27:51 +0700

    Commit

---
+++ src/id/ac/polbeng/yudiamrizal/test_kelas/Prak3_PersonE.kt
@@ -0,0 +1,26 @@
+package id.ac.polbeng.yudiamrizal.test_kelas
+
+class PersonE {
+    var firstName: String
+    var lastName: String
+    var age: Int = 25
+    constructor(_firstName: String, _lastName: String){
+        firstName = _firstName
+        lastName = _lastName
+    }
+    constructor(_firstName: String, _lastName: String, _age: Int): this(_firstName, _lastName){
+        age = _age
+    }
+    fun cetakInfo(){
+        println("Name : ${firstName} ${lastName}")
+        println("Age : ${age}")
+    }
+}
+
+fun main(){
+    val anton = PersonE("Frank", "Lampard")
+    anton.cetakInfo()
+    println()
+    val budi = PersonE("Budi", "Gunawan", 21)
+    budi.cetakInfo()
+}
