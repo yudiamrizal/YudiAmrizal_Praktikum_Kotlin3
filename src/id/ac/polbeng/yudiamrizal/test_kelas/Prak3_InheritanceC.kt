5c9103b4ce173bba0a763b86f45400186f1330ce
yudi amrizal <yudiamrizalpnb@gmail.com>
2025-10-01 13:55:58 +0700

    Commit

---
+++ src/id/ac/polbeng/yudiamrizal/test_kelas/Prak3_InheritanceC.kt
@@ -0,0 +1,27 @@
+package id.ac.polbeng.yudiamrizal.test_kelas
+
+open class Teacher {
+    // Must use "open" modifier to allow child classes to override it
+    open fun teach() {
+        println("Teaching...")
+    }
+
+    fun info(){
+        println("I'am a Teacher.")
+    }
+}
+
+class MathsTeacher : Teacher() {
+    // Must use "override" modifier to override a base class function
+    override fun teach() {
+        println("Teaching Maths...")
+    }
+}
+
+fun main() {
+    val teacher = Teacher()
+    val mathsTeacher = MathsTeacher()
+    mathsTeacher.info()
+    teacher.teach()
+    mathsTeacher.teach()
+}
